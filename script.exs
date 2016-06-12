7e13f5e13c797d23a41264ad957edb9006c49a9d
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-06-12 16:29:19 +0900

    Write a little

--- script.exs
+++ script.exs
@@ -1,5 +1,6 @@
 defmodule ChickenRace do
   use Xee.ThemeScript
+  require Logger
 
   # Callbacks
   def script_type do
@@ -10,18 +11,158 @@ defmodule ChickenRace do
 
   def init do
     {:ok, %{"data" => %{
+       participants: %{},
+       buyer_bids: [],
+       highest_bid: nil,
+       seller_bids: [],
+       lowest_bid: nil,
+       deals: [],
+       started: false,
      }}}
   end
 
-  def join(data, id) do
-    {:ok, %{"data" => data}}
+  def join(%{participants: participants} = data, id) do
+    if not Map.has_key?(participants, id) do
+      participant = %{role: nil, bidded: false, money: nil, bid: nil, dealt: false}
+      participants = Map.put(participants, id, participant)
+      data = %{data | participants: participants}
+      action = %{
+        type: "ADD_USER",
+        id: id,
+        user: participant
+      }
+      {:ok, %{"data" => data, "host" => %{action: action}}}
+    else
+      {:ok, %{"data" => data}}
+    end
   end
 
-  def handle_received(data, _received) do
-    {:ok, %{"data" => data}}
+  def handle_received(data, %{"action" => "start"}) do
+    {:ok, %{"data" => %{data | started: true}}}
   end
 
-  def handle_received(data, _action, _id) do
-    {:ok, %{"data" => data}}
+  def handle_received(data, %{"action" => "stop"}) do
+    {:ok, %{"data" => %{data | started: false}}}
+  end
+
+  def handle_received(%{participants: participants, buyer_bids: buyer_bids, seller_bids: seller_bids, deals: deals, highest_bid: highest_bid, lowest_bid: lowest_bid} = data, %{"action" => "bid", "params" => bid}, id) do
+    participant = Map.get(participants, id)
+    participant_actions = %{}
+    host_action = nil
+
+    case participant do
+      # Seller
+      %{role: :seller, bidded: false, money: money} when not is_nil(money) ->
+        if not is_nil(highest_bid) and bid <= elem(highest_bid, 1) do
+          now = DateTime.today()
+          buyer_id = elem(highest_bid, 0)
+          deals = [{bid, now, {id, buyer_id} | deals]
+          participants = participants
+                        |> Map.update!(id, &(Map.put(&1, :dealt, true)))
+                        |> Map.update!(buyer_id, &(Map.put(&1, :dealt, true)))
+          participant
+          buyer_bids = List.delete(buyer_bids, highest_bid)
+          data = %{data | deals: deals, participants: participants, buyer_bids: buyer_bids}
+          host_action = %{
+            type: "DEALT",
+            sellerID: id, buyerID: buyer_id, time: now, money: bid
+          }
+          participant_actions = Enum.map(participants, fn {p_id, _} ->
+            if p_id == id or p_id == buyer_id do
+              {id, %{
+                action: %{
+                  type: "DEALT",
+                  money: bid
+                }
+              }}
+            else
+              {id, %{
+                action: %{
+                  type: "SOMEONE_DEALT",
+                  money: bid
+                }
+              }}
+            end
+          end) |> Enum.into(%{})
+        else
+          seller_bids = [{id, bid} | seller_bids]
+          if is_nil(lowest_bid) or bid < elem(lowest_bid, 1) do
+            lowest_bid = {id, bid}
+          end
+          data = %{data | seller_bids: seller_bids, lowest_bid: lowest_bid}
+          host_action = %{
+            type: "NEW_SELLER_BIDS",
+            money: bid
+          }
+          participant_actions = Enum.map(participants, fn {p_id, _} ->
+            unless p_id == id do
+              {id, %{
+                action: %{
+                  type: "NEW_SELLER_BIDS",
+                  money: bid
+                }
+              }}
+            end
+          end) |> Enum.into(%{})
+        end
+      # Buyer
+      %{role: :buyer, bidded: false, money: money} when not is_nil(money) ->
+        if not is_nil(lowest_bid) and bid >= elem(lowest_bid, 1) do
+          seller_id = elem(lowest_bid, 0)
+          deals = [{bid, DateTime.today(), {id, seller_id} | deals]
+          participants = participants
+                        |> Map.update!(id, &(Map.put(&1, :dealt, true)))
+                        |> Map.update!(seller_id, &(Map.put(&1, :dealt, true)))
+          seller_bids = List.delete(seller_bids, lowest_bid)
+          data = %{data | deals: deals, participants: participants, seller_bids: seller_bids}
+          host_action = %{
+            type: "DEALT",
+            sellerID: id, buyerID: buyer_id, time: now, money: bid
+          }
+          participant_actions = Enum.map(participants, fn {p_id, _} ->
+            if p_id == seller_id or p_id == id do
+              {id, %{
+                action: %{
+                  type: "DEALT",
+                  money: bid
+                }
+              }}
+            else
+              {id, %{
+                action: %{
+                  type: "SOMEONE_DEALT",
+                  money: bid
+                }
+              }}
+            end
+          end) |> Enum.into(%{})
+        else
+          buyer_bids = [{id, bid} | buyer_bids]
+          if is_nil(highest_bihighest_bid) or  bid > elem(highest_bid, 1) do
+            highest_bid = {id, bid}
+          end
+          data = %{data | buyer_bids: buyer_bids, highest_bid: highest_bid}
+          host_action = %{
+            type: "NEW_BUYER_BIDS",
+            money: bid
+          }
+          participant_actions = Enum.map(participants, fn {p_id, _} ->
+            unless p_id == id do
+              {id, %{
+                action: %{
+                  type: "NEW_BUYER_BIDS",
+                  money: bid
+                }
+              }}
+            end
+          end) |> Enum.into(%{})
+        end
+    end
+
+    if not is_nil(host_action) do
+      {:ok, %{"data" => data, "host" => %{action: host_action}, "participant" => participant_actions}}
+    else
+      {:ok, %{"data" => data, "participant" => participant_actions}}
+    end
   end
 end
